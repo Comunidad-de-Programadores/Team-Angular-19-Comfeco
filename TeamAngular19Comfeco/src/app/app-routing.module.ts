a9cf279e3ec1c4f88ab047a6614a5e8839d6081d
Ervin <ervinsv92@gmail.com>
2021-02-15 21:48:49 -0600

    Se valida verificacion de correo para acceso a app

--- TeamAngular19Comfeco/src/app/app-routing.module.ts
+++ TeamAngular19Comfeco/src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import {RouterModule, Routes} from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './auth/login/login.component';
 import {RegisterComponent} from './auth/register/register.component';
+import { AuthGuard } from './guards/auth.guard';
 import {HomeComponent} from './pages/home/home.component';
 import {PagesComponent} from './pages/pages.component';
 
 const ROUTES: Routes = [
-  // {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
   { path: '',
     component: AuthComponent,
     children: [
@@ -19,12 +19,10 @@ const ROUTES: Routes = [
     path: 'private',
     component: PagesComponent,
     children: [
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent, canActivate:[AuthGuard] }
     ]
   },
-  // {path: 'register', component: RegisterComponent},
-  // {path: 'recover', component: RegisterComponent},
-  {path: '**', redirectTo: 'auth', pathMatch: 'full'}
+  {path: '**', redirectTo: '/login', pathMatch: 'full'}
 ];
 
 @NgModule({
